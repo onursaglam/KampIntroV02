8742595bfec37840471531fd5b82f6d29d339cc6
onursaglam <saglam-195@hotmail.com>
2021-02-09 17:48:22 +0300

    4.Ders odevler sonu

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K, T>
+    {
+        K[] keys;
+        T[] items;
+
+        public MyDictionary()
+        {
+            keys = new K[0];
+            items = new T[0];
+        }
+
+        public void Add(K key, T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+            K[] tempArray2 = keys;
+            keys = new K[keys.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                keys[i] = tempArray2[i];
+            }
+
+            keys[keys.Length - 1] = key;
+
+        }
+
+    }
+
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> dersler1 = new MyDictionary<int, string>();
+            dersler1.Add(01, "Matematik");
+            dersler1.Add(02, "Türkçe");
+            dersler1.Add(03, "Kimya");
+
+            MyDictionary<int, string> dersler2 = new MyDictionary<int, string>();
+            dersler2.Add(01, "Sosyal Bilgiler");
+            dersler2.Add(02, "Fen Bilgisi");
+            dersler2.Add(03, "Müzik");
+
+
+
+        }
+    }
+}
