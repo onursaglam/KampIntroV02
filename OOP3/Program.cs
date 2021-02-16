4774041f02e078318a70f9642c4ef1dc09e9f72e
onursaglam <saglam-195@hotmail.com>
2021-02-16 15:26:27 +0300

    .

---
+++ OOP3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IhtiyacKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+
+            //TasitKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+ 
+            //KonutKrediManager konutKrediManager = new KonutKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            //basvuruManager.BasvuruYap(ihtiyacKrediManager);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager,tasitKrediManager };
+            basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+        }
+    }
+}
